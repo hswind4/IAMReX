09c4848ab1fefaddb3158e0f8644c2d3737f3f46
hs <hswind53@gmail.com>
2025-01-20 19:24:08 +0300

    test5

--- Test_IAMReX/test_IAMReX.py
+++ Test_IAMReX/test_IAMReX.py
@@ -7,4 +7,15 @@ import subprocess
 # subprocess.run(["make", "-j8"], check=True)
 
 
-subprocess.run("cd ../Tutorials/LidDrivenCavity && make -j8 && ./amr2d.gnu.ex inputs.2d.lid_driven_cavity", shell=True, check=True)
+# subprocess.run("cd ../Tutorials/LidDrivenCavity && make -j8 && ./amr2d.gnu.ex inputs.2d.lid_driven_cavity", shell=True, check=True)
+home = os.getcwd() 
+
+working_dir = home + "/Tutorials/LidDrivenCavity"
+
+# 使用 cwd 参数指定工作目录
+subprocess.run(
+    "make -j8 && ./amr2d.gnu.ex inputs.2d.lid_driven_cavity", 
+    shell=True, 
+    check=True, 
+    cwd=working_dir
+)
